b4b33c8e5ebd478ce73fdfb1391ce025a20c788e
maharaferrarini <90833156+maharaferrarini@users.noreply.github.com>
2021-09-16 11:33:12 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("hihi")
+})
+radio.setGroup(7)
 basic.forever(function () {
-    radio.setGroup(7)
-    basic.showLeds(`
-        # . . . .
-        # # # . .
-        . . # . .
-        . . # # #
-        . . . . #
-        `)
+	
 })
